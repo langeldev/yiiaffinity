0106edaabe545f2844a03b1f981f2c5bf72679e9
Miguel Ángel Rodríguez Roldán <miguelangel.rodriguez.roldan@iesdonana.org>
2021-04-29 20:00:36 +0200

    Uso de jquery subir al cielo

--- web/js/main.js
+++ web/js/main.js
@@ -7,3 +7,19 @@
 function formateo(x) {
     return Number.parseFloat(x).toFixed(1).replace('.',',');
 }
+
+$(document).ready(function(){
+    $('#al-cielo').click(() =>{
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 600);
+    });
+
+    $(window).scroll(() => {
+        if($(this).scrollTop() > 200) {
+            $('#al-cielo').slideDown('slow');
+        } else {
+            $('#al-cielo').slideUp('slow');
+        }
+    });
+});
